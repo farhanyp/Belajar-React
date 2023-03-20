760d84fa87548daacf5cf6d6ab09c4cad236e1bc
farhanyp <36284284+farhanyp@users.noreply.github.com>
2023-03-20 15:16:03 +0700

    Statefull

---
+++ react-hello/src/project/Statefull/statefull.jsx
@@ -0,0 +1,12 @@
+// Statefull component memiliki banyak fitur, kompleks logika ui dan smart
+
+import React from "react";
+
+class Statefull extends React.Component{
+    render(){
+        return <p>Ini statefull component</p>
+    }
+}
+
+
+export default Statefull
