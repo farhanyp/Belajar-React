cd5336dd6cdb1412d7f784831a25ef9ee5c1d4f2
farhanyp <36284284+farhanyp@users.noreply.github.com>
2023-03-27 07:27:27 +0700

    CRUD

--- react-hello/src/project/BlogPost/BlogPost.jsx
+++ react-hello/src/project/BlogPost/BlogPost.jsx
@@ -5,25 +5,88 @@ import Post from "./Post"
 
 class BlogPost extends React.Component{
     state = {
-        post: []
+        post: [],
+        blogPostForm: {
+            userId:1,
+            id:"",
+            title:"",
+            body:""
+        },
+        isUpdate: false
     }
 
-    getDataAPI = () => {
+    getDataAPI = async () => {
         // Menggunakan Axios
-        axios.get('http://localhost:3004/posts').then((res) => {
+        await axios.get('http://localhost:3004/posts?_sort=id&_order=desc').then((res) => {
+                this.setState({
+                    post: res.data
+                })
+            })
+    }
+
+    postDataToAPI = () => {
+        axios.post('http://localhost:3004/posts/', this.state.blogPostForm).then(() => this.getDataAPI().then(() =>{
             this.setState({
-                post: res.data
+                blogPostForm: {
+                    userId:1,
+                    id:"",
+                    title:"",
+                    body:""
+                }
+            })
+        }), 
+        (error)=>{
+            console.log(error)
+        })
+    }
+
+    putDataToAPI = () =>{
+        axios.put(`http://localhost:3004/posts/${this.state.blogPostForm.id}`, this.state.blogPostForm).then(() => this.getDataAPI()).then(() => {
+            this.setState({
+                blogPostForm: {
+                    userId:1,
+                    id:"",
+                    title:"",
+                    body:""
+                }
             })
         })
     }
 
+    handleEvent = (event) =>{
+        let blogPostFormNew = {...this.state.blogPostForm}
+        const timestamp = new Date().getTime()
+        if(!this.state.isUpdate){
+            blogPostFormNew['id'] = timestamp
+        }
+        blogPostFormNew[event.target.name]=event.target.value
+        this.setState({
+            blogPostForm: blogPostFormNew
+        })
+    }
+
     handleRemove = (id) =>{
-        axios.delete(`http://localhost:3004/posts/${id}`).then((data) => console.log(data))
-        this.getDataAPI()
+        axios.delete(`http://localhost:3004/posts/${id}`).then(()=>this.getDataAPI())
+        
+    }
+
+    handleUpdate = (data) =>{
+        this.setState({
+            blogPostForm: data,
+            isUpdate: true
+        })
+    }
+
+    handleSubmit = () => {
+        if(this.state.isUpdate){
+            this.putDataToAPI()
+        }else{
+            this.postDataToAPI()
+        }
     }
 
     componentDidMount(){
-        
+
         this.getDataAPI()
 
         // Menggunakan Fetch
@@ -40,10 +103,17 @@ class BlogPost extends React.Component{
     render(){
         return(
             <>
-            <p className="section-title"></p>
+            <p className="section-title">Blog Post</p>
+            <div className="form-add-post">
+                <label htmlFor="title">Title</label>
+                <input type="text" name="title" value={this.state.blogPostForm.title} placeholder="add title" onChange={this.handleEvent}/>
+                <label htmlFor="body">Blog Content</label>
+                <textarea name="body" id="body" cols="30" rows="10" placeholder="add body" onChange={this.handleEvent} value={this.state.blogPostForm.body}></textarea>
+                <button className="btn-submit" onClick={this.handleSubmit}>Simpan</button>
+            </div>
             {
             this.state.post.map((post) => {
-                return <Post key={post.id} data={post} remove = {this.handleRemove}/>
+                return <Post key={post.id} data={post} remove = {this.handleRemove} update = {this.handleUpdate}/>
             })
             }
             
